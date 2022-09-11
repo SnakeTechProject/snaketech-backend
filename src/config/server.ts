608a3c30de353fa4b52603fac2f8408373cfe6ba
hanyelC <hanyel.chamon@gmail.com>
2022-09-10 21:44:39 -0300

    feat: add server config

---
+++ src/config/server.ts
@@ -0,0 +1,11 @@
+import { routes } from './../routes/index';
+import express from 'express';
+import cors from 'cors';
+
+export const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.use(routes);
