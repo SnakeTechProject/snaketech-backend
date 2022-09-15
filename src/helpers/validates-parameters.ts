5fbab58e44c842a49fc19cde05b60e91a473d52f
Vinicius Cosmo <viniciuscosmott+github@gmail.com>
2022-09-15 15:52:25 -0300

    feat: adds email validation

--- src/helpers/validates-parameters.ts
+++ src/helpers/validates-parameters.ts
@@ -4,19 +4,22 @@ interface ILengthLimit {
 }
 
 class Validate {
-  public isNotEmpty = (param: string | number): boolean => {
+  private static uuidV4RegEx = /^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i;
+  private static emailRegEx = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+
+  public static isNotEmpty = (param: string | number): boolean => {
     return !(param == null || param == undefined || param === '');
   };
 
-  public isString = (param: string): boolean => {
+  public static isString = (param: string): boolean => {
     return (typeof param == 'string' && typeof param != 'number');
   };
 
-  public isNumber = (param: number): boolean => {
+  public static isNumber = (param: number): boolean => {
     return (!isNaN(param) && typeof param == 'number');
   };
 
-  public contentLengthLimit = (param: string | Array<string>, {min = 0, max = 0}: ILengthLimit): boolean => {
+  public static contentLengthLimit = (param: string | Array<string>, {min = 0, max = 0}: ILengthLimit): boolean => {
     if (!Array.isArray(param) && !this.isString(param)) return !1;
 
     return (max > min)
@@ -24,12 +27,16 @@ class Validate {
       : (param.length >= min);
   };
 
-  public id = (id: number): boolean => {
+  public static id = (id: number): boolean => {
     return (this.isNotEmpty(id) && this.isNumber(id));
   };
 
-  public uuid = (uuid: string): boolean => {
-    return (this.isNotEmpty(uuid) && this.isString(uuid));
+  public static uuid = (uuid: string): boolean => {
+    return (this.uuidV4RegEx.test(uuid));
+  };
+
+  public static email = (email: string): boolean => {
+    return (this.emailRegEx.test(email));
   };
 }
 
