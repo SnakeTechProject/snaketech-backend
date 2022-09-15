3c7d20c187d303537385a7214387272b2b5cea5a
hanyelC <hanyel.chamon@gmail.com>
2022-09-15 16:47:50 -0300

    fix: handling errors

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,3 +1,4 @@
+import { errorMiddleware } from './../middlewares/errorMiddleware';
 import { routes } from './../routes/index';
 import express from 'express';
 import cors from 'cors';
@@ -9,3 +10,5 @@ app.use(express.json());
 app.use(cors());
 
 app.use(routes);
+
+app.use(errorMiddleware);
