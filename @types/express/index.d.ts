b536c1000003d0c724b91090a682dffe34f8d2dd
hanyelC <hanyel.chamon@gmail.com>
2022-09-15 16:25:24 -0300

    chore: add global types

---
+++ @types/express/index.d.ts
@@ -0,0 +1,13 @@
+/* eslint-disable no-var */
+import { PrismaClient } from '@prisma/client';
+import { Request } from 'express';
+
+declare global {
+  var prisma: PrismaClient | null;
+
+  interface CustomRequest extends Request {
+    user?: {
+      user_id: string;
+    };
+  }
+}
