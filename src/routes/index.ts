5c64ac4e586ecc1b5329b87c9d4942651521c998
hanyelC <hanyel.chamon@gmail.com>
2022-09-15 16:41:37 -0300

    feat: create userRouter

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import { rootRouter } from './root.routes';
+import { usersRouter } from './users.routes';
 
 export const routes = Router();
 
+routes.use('/users', usersRouter);
 routes.use('/', rootRouter);
