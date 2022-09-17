eec15e28bdf9fa7e268216051e32402c98664075
Eric Oliveira <oiericoi@hotmail.com>
2022-09-17 15:46:47 -0300

    refactor: refactor article repository

--- src/modules/articles/repositories/articleRepository.ts
+++ src/modules/articles/repositories/articleRepository.ts
@@ -1,35 +1,44 @@
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
-
-interface IArticle {
-  title: string;
-  slug: string;
-  content: string;
-  author_id: string;
-}
+import { prisma as db } from '../../../lib/prisma';
+import { IArticle, IArticleUpdate } from '../../../interfaces/articleInterface';
 
 export class ArticleRepository {
   async create(article: IArticle) {
-    return await prisma.article.create({ data: article });
+    return await db.article.create({
+      data: article
+    });
   }
 
   async findOneById(id: number) {
-    return await prisma.article.findUnique({ where: { id: id } });
+    return await db.article.findUnique({
+      where: { id },
+      include: {
+        author: true,
+        Comment: true,
+        Likes: true,
+      }
+    });
   }
 
   async findAll() {
-    return await prisma.article.findMany();
+    return await db.article.findMany({
+      include: {
+        author: true,
+        Comment: true,
+        Likes: true,
+      }
+    });
   }
 
-  async update(id: number, data: IArticle) {
-    return await prisma.article.update({
-      where: { id: id },
+  async update(id: number, data: IArticleUpdate) {
+    return await db.article.update({
+      where: { id },
       data: data
     });
   }
 
   async delete(id: number) {
-    return await prisma.article.delete({ where: { id: id } });
+    return await db.article.delete({
+      where: { id }
+    });
   }
 }
