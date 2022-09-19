66f910aed2436adfa405732c7e2c9e05c973fb8e
hanyelC <hanyel.chamon@gmail.com>
2022-09-19 19:40:17 -0300

    feat: add routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
+import { authenticationRouter } from './authentication.routes';
 import { rootRouter } from './root.routes';
 import { usersRouter } from './users.routes';
 
 export const routes = Router();
 
 routes.use('/users', usersRouter);
+routes.use('/sessions', authenticationRouter);
 routes.use('/', rootRouter);
