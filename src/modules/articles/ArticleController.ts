9e53df5b1d8b802214d8dcd7653290b62a619c1d
programeiro <dansenpirdev@gmail.com>
2022-09-20 20:48:39 -0300

    feat: adds read method to ArticleController

--- src/modules/articles/ArticleController.ts
+++ src/modules/articles/ArticleController.ts
@@ -2,18 +2,32 @@ import { NextFunction, Request, Response } from 'express';
 import { IArticle } from '../../interfaces/articleInterface';
 import { ArticleRepository } from './repositories/ArticleRepository';
 import CreateArticle from './useCase/CreateArticle';
+import ReadArticle from './useCase/ReadArticle';
 
+const repository = new ArticleRepository();
 export class ArticleController {
   async create(req: Request, res: Response, next: NextFunction) {
     const { author_id, content, title } = req.body;
 
-    const repository = new ArticleRepository();
-    const createService = new CreateArticle(repository);
+    const createArticle = new CreateArticle(repository);
 
     try {
-      await createService.execute({ author_id, content, title } as IArticle);
+      await createArticle.execute({ author_id, content, title } as IArticle);
 
       return res.sendStatus(201);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async read(req: Request, res: Response, next: NextFunction) {
+    const id = parseInt(req.params.id);
+
+    const readArticle = new ReadArticle(repository);
+
+    try {
+      const response = await readArticle.execute(id);
+      return res.json(response);
     } catch (error) {
       next(error);
     }
