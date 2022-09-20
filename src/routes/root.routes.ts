16a90b27526457488ab6b9cbe87c9d1dd88a4729
programeiro <dansenpirdev@gmail.com>
2022-09-20 11:22:43 -0300

    feat: adds article create route

--- src/routes/root.routes.ts
+++ src/routes/root.routes.ts
@@ -1,6 +1,13 @@
 import { Router, Request, Response } from 'express';
+import { ArticleController } from '../modules/articles/ArticleController';
 
 export const rootRouter = Router();
+
+const articleController = new ArticleController();
+
+rootRouter.post('/blog', (req: Request, res: Response) => {
+  articleController.create(req, res);
+});
 
 rootRouter.use('/', (req: Request, res: Response) => {
   res.send('SnakeTech Api');
