ca98b3d2bf36efd9b739f986531a1526ac9a7903
programeiro <dansenpirdev@gmail.com>
2022-09-20 21:15:15 -0300

    feat: adds findAll method to ArticleController

--- src/modules/articles/ArticleController.ts
+++ src/modules/articles/ArticleController.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 import { IArticle } from '../../interfaces/articleInterface';
 import { ArticleRepository } from './repositories/ArticleRepository';
 import CreateArticle from './useCase/CreateArticle';
+import FindAllArticles from './useCase/FindAllArticles';
 import ReadArticle from './useCase/ReadArticle';
 
 const repository = new ArticleRepository();
@@ -27,6 +28,17 @@ export class ArticleController {
 
     try {
       const response = await readArticle.execute(id);
+      return res.json(response);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async findAll(res: Response, next: NextFunction) {
+    const findAllArticles = new FindAllArticles(repository);
+
+    try {
+      const response = await findAllArticles.execute();
       return res.json(response);
     } catch (error) {
       next(error);
