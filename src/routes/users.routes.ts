c2ddf36cfd29e73edf248f1247431f8884ebd79c
Eric Oliveira <oiericoi@hotmail.com>
2022-09-23 17:27:36 -0300

    feat: add delete mmethod in user routes

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -10,3 +10,5 @@ usersRouter.get('/find-by-id', UsersController.read);
 usersRouter.get('/find-by-email', UsersController.findByEmail);
 
 usersRouter.put('/update/:id', UsersController.update);
+
+usersRouter.delete('/delete-by-id', UsersController.delete);
