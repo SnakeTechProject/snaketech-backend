7e62047641db631fa89894e7559e58e680842ea1
viniciuscosmome <viniciuscosmosp@gmail.com>
2022-09-23 16:13:11 -0300

    feat(repository): adds findOneById function and changes IComment interface

--- src/modules/comments/repositories/CommentsRepository.ts
+++ src/modules/comments/repositories/CommentsRepository.ts
@@ -1,22 +1,16 @@
-import { prisma as db, IComment } from '../../../lib/prisma';
+import { prisma as db } from '../../../lib/prisma';
+import { IComment } from '../../../interfaces/commentInterface';
 
 export class CommentsRepository {
-  async create({author_id, article_id, parent_id, content, created_at }: IComment) {
-    await db.comment.create({
-      data: {
-        content,
-        author_id,
-        article_id,
-        parent_id,
-        created_at
-      },
-    });
+  async create(data: IComment) {
+    await db.comment.create({data});
   }
 
   async findAllByArticleId(article_id: number) {
     const comments = await db.comment.findMany({
       where: {
         article_id,
+        parent_id: null
       },
       select: {
         id: true,
@@ -39,10 +33,36 @@ export class CommentsRepository {
     return comments;
   }
 
-  async update({id, content}: IComment) {
-    await db.comment.update({
+  async findOneById(id: number) {
+    const comment = await db.comment.findFirst({
       where: {
         id,
+      },
+      select: {
+        id: true,
+        author_id: true,
+        parent_id: true,
+        content: true,
+        created_at: true,
+        reply: {
+          select: {
+            id: true,
+            author_id: true,
+            parent_id: true,
+            content: true,
+            created_at: true,
+          }
+        }
+      }
+    });
+
+    return comment;
+  }
+
+  async update(id: number, content: string) {
+    await db.comment.update({
+      where: {
+        id
       },
       data: {
         content,
