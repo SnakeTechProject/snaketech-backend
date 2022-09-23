5a473fc81be3518acdee92fefb59698d1fd020e4
hanyelC <hanyel.chamon@gmail.com>
2022-09-22 22:59:23 -0300

    feat: create authorization routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import { authenticationRouter } from './authentication.routes';
 import { blogRouter } from './blog.routes';
+import { permissionRouter } from './permission.routes';
+import { rolesRouter } from './roles.routes';
 import { rootRouter } from './root.routes';
 import { usersRouter } from './users.routes';
 
@@ -9,4 +11,6 @@ export const routes = Router();
 routes.use('/blog', blogRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', authenticationRouter);
+routes.use('/roles', rolesRouter);
+routes.use('/permissions', permissionRouter);
 routes.use('/', rootRouter);
