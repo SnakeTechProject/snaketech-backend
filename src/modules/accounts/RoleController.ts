4757039b7b66cc42a0a2c90d5da1ce43480c0ee1
hanyelC <hanyel.chamon@gmail.com>
2022-09-22 22:57:57 -0300

    feat: create the RoleController

---
+++ src/modules/accounts/RoleController.ts
@@ -0,0 +1,16 @@
+import { Response } from 'express';
+import { RoleRepository } from './repositories/RoleRepository';
+import { CreateRole } from './useCases/CreateRole';
+
+export class RoleController {
+  static async create(req: CustomRequest, res: Response) {
+    const { name, description } = req.body;
+
+    const roleRepository = new RoleRepository();
+    const useCase = new CreateRole(roleRepository);
+
+    await useCase.execute(name, description);
+
+    return res.sendStatus(201);
+  }
+}
