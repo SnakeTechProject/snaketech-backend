ff86fda2e661be48207d39fa5c021e2fff2cd67e
Eric Oliveira <oiericoi@hotmail.com>
2022-09-23 15:31:46 -0300

    feat: add update  method in user routes

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -8,3 +8,5 @@ usersRouter.post('/', UsersController.create);
 usersRouter.get('/find-by-id', UsersController.read);
 
 usersRouter.get('/find-by-email', UsersController.findByEmail);
+
+usersRouter.put('/update/:id', UsersController.update);
