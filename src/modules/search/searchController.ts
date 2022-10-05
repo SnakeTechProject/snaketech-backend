dc9a61a1cd99e14c43927252f0c76d44167853ff
Eric Oliveira <88443797+EricOliveiras@users.noreply.github.com>
2022-10-05 11:18:03 -0300

    refactor: modify search controller

--- src/modules/search/searchController.ts
+++ src/modules/search/searchController.ts
@@ -1,17 +1,25 @@
 import { Request, Response } from 'express';
 import { ArticleRepository } from '../articles/repositories/ArticleRepository';
+import { ChanllengeRepository } from '../challenges/repositories/ChallengeRepository';
 import { FindArticles } from './useCases/FindArticles';
+import { FindChallenges } from './useCases/FindChallenges';
 
-const repository = new ArticleRepository();
+const articleRepository = new ArticleRepository();
+const challengeRepository = new ChanllengeRepository();
 
 export class SearchController {
   static async find(req: Request, res: Response) {
     const { parameters } = req.body;
 
-    const useCase = new FindArticles(repository);
+    const useCaseArticles = new FindArticles(articleRepository);
+    const useCaseChallenges = new FindChallenges(challengeRepository);
 
-    const articles = await useCase.execute(parameters);
+    const articles = await useCaseArticles.execute(parameters);
+    const challenges = await useCaseChallenges.execute(parameters);
 
-    return res.status(200).json(articles);
+    return res.status(200).json({
+      articles,
+      challenges
+    });
   }
 }
