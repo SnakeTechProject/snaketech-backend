ca4e9aa7190927c752799f78ffa6ffb39204668b
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2022-10-07 09:54:22 -0300

    feat: creates like interfaces

--- src/interfaces/likeInterface.ts
+++ src/interfaces/likeInterface.ts
@@ -3,3 +3,20 @@ export interface ILike {
   fk_article_id: number;
   is_liked: boolean;
 }
+
+export interface ILikeDelete {
+  fk_user_id: string;
+  fk_article_id: number;
+}
+
+export interface ILikeQuery {
+  fk_user_id?: string;
+  fk_article_id: number;
+  is_liked?: boolean;
+}
+
+export interface ILikeResponse {
+  like: number;
+  dislike: number;
+  user_liked: boolean;
+}
